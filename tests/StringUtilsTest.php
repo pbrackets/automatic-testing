08cf8904bd50ef39227a669d389f4cdfa7c4295c
Aelita <pbrackets@yandex.ru>
2022-09-14 17:40:00 +0500

    test1

---
+++ tests/StringUtilsTest.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Нажмите кнопку run чтобы запустить тесты.
+Попробуйте изменять код функции / тестов, запуская проверки заново.
+*/
+
+require("src/StringUtils.php");
+
+if (StringUtils\capitalize('hello') !== 'Hello') {
+    throw new \Exception('Функция работает неверно!');
+}
+
+if (StringUtils\capitalize('') !== '') {
+    throw new \Exception('Функция работает неверно!');
+}
+
+echo 'Все тесты пройдены!';
